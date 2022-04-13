82fd7bda3552648d3ccc8122be5c494f76e95c04
William Abate <williamabate6@gmail.com>
2022-04-13 18:04:48 -0500

    Update Main.py

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -20,16 +20,43 @@ budget_data = os.path.join("Resources","budget_data.csv")
 #VARIABLES:
 monthtot = []
 profloss = []
+monthtot_count = 0.0
 plchange = 0.0
 totalprof = 0.0
 prolos_ch = []
-print("Financial Analysis")
-print("----------------------------")
+
 
 #Open CSV & Read
-with open(budget_csv, newline="") as csvfile:
+with open(budget_data.csv, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimeter=",")
     csv_header = next(csvfile)
 
 #count toal number of months
+    for row in csv_reader:
+        monthtot_count += 1
+        monthtot.append(row[0])
+        profloss.append(row[1])
 
+#Total for profit and losses over entire dataset
+    for number in profloss:
+        totalprof = totalprof + int(number)
+
+#Calculate the changes in "Profit/Losses" over the entire period, 
+    for number in range(len(profloss)-1):
+        profloss.append(int(profloss[number]-1)-int(profloss[number]))
+
+    sum_profloss = 0
+    for number in profloss:
+        sum_profloss = sum_profloss + number
+
+#then find the average of those changes
+    averagechange = round(sum_profloss / (totalprof-1))
+
+
+#Print
+print("Financial Analysis")
+print("----------------------------")
+print(f"Header: {csv_header}")
+print(f"Total Months: {monthtot_count}")
+print(f"Total: ${totalprof}")
+print(f"Average Change: ${averagechange}")
